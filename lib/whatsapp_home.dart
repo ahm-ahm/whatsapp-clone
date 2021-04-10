042a8e3b9786d5ae3b200f5f7fd839ccaa2f010f
unknown <ahm.ahm7785@gmail.com>
2021-04-10 11:17:21 +0500

    first commit

---
+++ lib/whatsapp_home.dart
@@ -0,0 +1,78 @@
+import 'package:camera/camera.dart';
+import 'package:flutter/material.dart';
+import 'package:whats_app/pages/call_screen.dart';
+import 'package:whats_app/pages/camera_screen.dart';
+import 'package:whats_app/pages/chat_screen.dart';
+import 'package:whats_app/pages/status_screen.dart';
+
+class WhatsAppHome extends StatefulWidget {
+  final List<CameraDescription> cameras;
+  WhatsAppHome({this.cameras});
+
+  @override
+  _WhatsAppHomeState createState() => _WhatsAppHomeState();
+}
+
+class _WhatsAppHomeState extends State<WhatsAppHome>
+    with SingleTickerProviderStateMixin {
+  TabController _tabController;
+  bool showFab = true;
+
+  @override
+  void initState() {
+    super.initState();
+
+    _tabController = TabController(vsync: this, initialIndex: 1, length: 3);
+    _tabController.addListener(() {
+      if (_tabController.index == 1) {
+        showFab = true;
+      } else {
+        showFab = false;
+      }
+      setState(() {});
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("WhatsApp"),
+        elevation: 0.7,
+        bottom: TabBar(
+          controller: _tabController,
+          indicatorColor: Colors.white,
+          tabs: <Widget>[
+            Tab(icon: Icon(Icons.camera_alt)),
+            Tab(text: "CHATS"),
+            // Tab(
+            //   text: "STATUS",
+            // ),
+            Tab(
+              text: "CALLS",
+            ),
+          ],
+        ),
+      ),
+      body: TabBarView(
+        controller: _tabController,
+        children: <Widget>[
+          CameraScreen(widget.cameras),
+          ChatScreen(),
+          // StatusScreen(),
+          CallsScreen(),
+        ],
+      ),
+      floatingActionButton: showFab
+          ? FloatingActionButton(
+              backgroundColor: Theme.of(context).accentColor,
+              child: Icon(
+                Icons.message,
+                color: Colors.white,
+              ),
+              onPressed: () => print("open chats"),
+            )
+          : null,
+    );
+  }
+}
